4b1cee0bc820e3236b357fce86a710ac785e6090
Cayas Diaz Abraham <acadi2107@gmail.com>
2026-09-21 21:50:45 -0500

    cambio NICOLE

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Para mi mejor amiga Pandy').split('')
+    const titles = ('Que tengas un bonito dia').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
